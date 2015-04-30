79734620851de648815035c70fc99e3f351039d7
Raoul DIFFOUO <diraulo@gmail.com>
2015-05-01 00:18:02 +0200

    Add api constraints specs
    - add api constraints specs
    - implement ApiConstraints class
    - add api constraints to route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  # Api definition
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, path: '/' do
-
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      # Add list of resources here
+    end
   end
 end
