ce2fb164205a64976eaf61bf16a13e8da8fe15cb
Raoul DIFFOUO <diraulo@gmail.com>
2015-05-13 21:29:23 +0200

    Add sessions controller <destroy> action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -16,6 +16,13 @@ module API
           render json: { errors: 'Invalid email or password' }, status: 422
         end
       end
+
+      def destroy
+        user = User.find_by(auth_token: params[:id])
+        user.generate_authentication_token!
+        user.save
+        head 204
+      end
     end
   end
 end
