8775836c11e392e964eb496db1bcb597f1f2a5b2
Raoul DIFFOUO <diraulo@gmail.com>
2015-05-17 13:16:12 +0200

    refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,9 @@ class User < ActiveRecord::Base
   validates :auth_token, uniqueness: true
 
   def generate_authentication_token!
-    # loop do
-    #   self.auth_token = Devise.friendly_token
-    #   break unless self.class.exists?(auth_token: auth_token)
-    # end
-
-    begin
+    loop do
       self.auth_token = Devise.friendly_token
-    end while self.class.exists?(auth_token: auth_token)
+      break unless self.class.exists?(auth_token: auth_token)
+    end
   end
 end
